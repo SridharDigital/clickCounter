0a84d4c541a87ac8ecc24ed3f7d3f0798be37325
Sreedharan Namboodiri C <sridhardigital22@gmail.com>
2022-02-16 13:12:16 +0000

    app is build but with error

--- src/components/ClickCounter/index.js
+++ src/components/ClickCounter/index.js
@@ -1,1 +1,28 @@
-// Write your code here
+import {Components} from 'react'
+import './index.css'
+
+class ClickCounter extends Components {
+  state = {count: 0}
+
+  onIncrement = () => {
+    this.setState(prevState => ({count: prevState.count + 1}))
+  }
+
+  render() {
+    const {count} = this.state
+    return (
+      <div className="bg-container">
+        <h1 className="heading">
+          The Button has been clicked <span className="counter">{count}</span>{' '}
+          times
+        </h1>
+        <p className="description">Click the button to increase the count!</p>
+        <button className="button" type="button">
+          Click Me!
+        </button>
+      </div>
+    )
+  }
+}
+
+export default ClickCounter
